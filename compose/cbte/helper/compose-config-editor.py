7d1c3fd54c4208536b306f0db324ec4e6639f445
Greg Miller <ggxedq@gmail.com>
2024-04-15 13:27:54 +0400

    #1042 Changed py helper

--- compose/cbte/helper/compose-config-editor.py
+++ compose/cbte/helper/compose-config-editor.py
@@ -35,17 +35,17 @@ certbot_config = {
 
 
 nginx_http_volumes = [
-	"./nginx/nginx.http.conf.template:/etc/nginx/conf.d/nginx.conf",
+	"./nginx/nginx.http.conf.template:/etc/nginx/conf.d/default.conf",
 	"./nginx/cloudbeaver.locations:/etc/nginx/conf.d/cloudbeaver.locations"
 	]
 nginx_ssl_volumes = [
-	"./nginx/nginx.https.conf.template:/etc/nginx/conf.d/nginx.conf",
+	"./nginx/nginx.https.conf.template:/etc/nginx/conf.d/default.conf",
 	"./nginx/cloudbeaver.locations:/etc/nginx/conf.d/cloudbeaver.locations",
 	"./nginx/ssl:/etc/nginx/ssl"
 	]
 nginx_le_volumes = [
 	"./nginx/cloudbeaver.locations:/etc/nginx/conf.d/cloudbeaver.locations",
-	"./nginx/nginx.le.conf.template:/etc/nginx/conf.d/nginx.conf",
+	"./nginx/nginx.le.conf.template:/etc/nginx/conf.d/default.conf",
 	"./data/certbot/conf:/etc/letsencrypt",
 	"./data/certbot/www:/var/www/certbot"
 	]
